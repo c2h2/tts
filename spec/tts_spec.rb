49d2cd3b7c047219075d7a55d9664eecbc7463c4
Qichunren 曹金华 <whyruby@gmail.com>
2011-06-16 17:23:54 +0800

    delete generated mp3 files after executed test.

--- spec/tts_spec.rb
+++ spec/tts_spec.rb
@@ -11,11 +11,13 @@ describe 'to_file method' do
   it "to_file should generate a mp3 file for a correct string" do
     "hello".to_file("en")
     File.exist?("hello.mp3").should be_true
+    File.delete("hello.mp3")
   end
   
   it "to_file should generate a mp3 file with given name for a correct string" do
     "hello".to_file("en", "my_hello.mp3")
     File.exist?("my_hello.mp3").should be_true
+    File.delete("my_hello.mp3")
   end
   
 end
