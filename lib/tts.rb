a46331562995835bf6fc8d744e1be3a8ce021dfb
Qichunren 曹金华 <whyruby@gmail.com>
2011-06-16 17:12:42 +0800

    refact [write file] code.

--- lib/tts.rb
+++ lib/tts.rb
@@ -6,15 +6,15 @@ module Tts
     base = "http://translate.google.com/translate_tts?tl=#{lang}&q=#{URI.escape self}"
   end  
 
-  def to_file lang, fn=nil
+  def to_file lang, file_name=nil
     require 'open-uri'
-    fn = self +".mp3" if fn.nil?
+    file_name = self +".mp3" if file_name.nil?
     url = self.to_url(lang)
     ua = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.68 Safari/534.24"
     content = open(url, "User-Agent" => ua).read
-    fh = open(fn, "wb")
-    fh.write content
-    fh.close
+    File.open(file_name, "wb") do |f|
+      f.puts content
+    end
   end
 end 
 
