425b0aaa948317bcf4509468fba910c1432570d4
Yiling Cao <yiling.cao@gmail.com>
2011-06-16 16:27:45 +0800

    finished dl file.

--- lib/tts.rb
+++ lib/tts.rb
@@ -5,8 +5,15 @@ module Tts
     base = "http://translate.google.com/translate_tts?tl=#{lang}&q=#{self}"
   end  
 
-  def to_file
-
+  def to_file lang, fn=nil
+    require 'open-uri'
+    fn = self +".mp3" if fn.nil?
+    url = self.to_url(lang)
+    ua = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.68 Safari/534.24"
+    content = open(url, "User-Agent" => ua).read
+    fh = open(fn, "wb")
+    fh.write content
+    fh.close
   end
 end 
 
