31e1f2797b30abb6611fbe0ea5076df23e4a7c9c
Qichunren 曹金华 <whyruby@gmail.com>
2011-06-16 17:16:55 +0800

    add spec test.

--- spec/tts_spec.rb
+++ spec/tts_spec.rb
@@ -1,8 +1,21 @@
 require File.expand_path("../../lib/tts", __FILE__)
 require "rspec"
 
-describe 'testing' do
+describe 'to_url method' do
   it "to_url should return a correct string" do
     "hello".to_url("en").should == "http://translate.google.com/translate_tts?tl=en&q=hello"
   end
+end
+
+describe 'to_file method' do
+  it "to_file should generate a mp3 file for a correct string" do
+    "hello".to_file("en")
+    File.exist?("hello.mp3").should be_true
+  end
+  
+  it "to_file should generate a mp3 file with given name for a correct string" do
+    "hello".to_file("en", "my_hello.mp3")
+    File.exist?("my_hello.mp3").should be_true
+  end
+  
 end
