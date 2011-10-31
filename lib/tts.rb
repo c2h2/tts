13217cb8cabb7a163ef14be859a90c40a455cbd9
Qichunren 曹金华 <whyruby@gmail.com>
2011-10-31 15:11:53 +0800

    add Tts.server_url(another_url) method to set anther tts server host.

--- lib/tts.rb
+++ lib/tts.rb
@@ -5,13 +5,19 @@ def to_valid_fn fn
 end
 
 
-module Tts  
+module Tts
+  @@default_url = "http://translate.google.com/translate_tts"
+  def self.server_url url=nil
+    return @@default_url if url.nil?
+    @@default_url = url
+  end
+
   def to_url lang
     require 'uri'
     langs = ["zh", "en", "it", "fr"]
     #raise "Not accepted language, accpeted are #{langs * ","}" unless langs.include? lang
-    base = "http://translate.google.com/translate_tts?tl=#{lang}&q=#{URI.escape self}"
-  end  
+    base = "#{Tts.server_url}?tl=#{lang}&q=#{URI.escape self}"
+  end
 
   def to_file lang, file_name=nil
     require 'open-uri'
@@ -20,7 +26,7 @@ module Tts
     ua = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.68 Safari/534.24"
     begin
       content = open(url, "User-Agent" => ua).read
-      
+
       File.open(file_name, "wb") do |f|
         f.puts content
       end
@@ -30,8 +36,11 @@ module Tts
     end
 
   end
-end 
+end
 
 class String
+
+
+
   include Tts
 end
--- spec/tts_spec.rb
+++ spec/tts_spec.rb
@@ -3,43 +3,45 @@ require File.expand_path("../../lib/tts", __FILE__)
 require "rspec"
 
 describe "to_valid_fn method" do
+  # Tts.server_url  "http://127.0.0.1:3001/translate_tts"
+
   # fn.gsub(/[\x00\/\\:\*\?\"<>\|]/, '_')
   it "should replace * with _" do
     to_valid_fn("hello*nice").should == "hello_nice"
     to_valid_fn("hello*nice*hello").should == "hello_nice_hello"
   end
-  
+
   it "should replace / with _" do
     to_valid_fn("hello/nice").should == "hello_nice"
     to_valid_fn("hello/nice/hello").should == "hello_nice_hello"
   end
-  
+
   it "should replace / with _" do
     to_valid_fn("hello:nice").should == "hello_nice"
     to_valid_fn("hello:nice:hello").should == "hello_nice_hello"
   end
-  
+
   it "should replace / with _" do
     to_valid_fn("hello?nice").should == "hello_nice"
     to_valid_fn("hello?nice?hello").should == "hello_nice_hello"
   end
-  
+
   it "should replace / with _" do
     to_valid_fn("hello|nice").should == "hello_nice"
     to_valid_fn("hello|nice?hello").should == "hello_nice_hello"
   end
-  
-end 
+
+end
 
 describe 'to_url method' do
   it "to_url should return a correct string" do
     "hello".to_url("en").should == "http://translate.google.com/translate_tts?tl=en&q=hello"
   end
-  
+
   it "to_url should return a correct string with chinese char" do
     "人民广场".to_url("zh").should == "http://translate.google.com/translate_tts?tl=zh&q=%E4%BA%BA%E6%B0%91%E5%B9%BF%E5%9C%BA"
   end
-  
+
 end
 
 describe 'to_file method' do
@@ -48,17 +50,30 @@ describe 'to_file method' do
     File.exist?("hello.mp3").should be_true
     File.delete("hello.mp3")
   end
-  
+
   it "to_file should generate a mp3 file with given name for a correct string" do
     "hello".to_file("en", "my_hello.mp3")
     File.exist?("my_hello.mp3").should be_true
     File.delete("my_hello.mp3")
   end
-  
+
   it "to_file should generate a mp3 file for a correct chinese string" do
     "人民广场到了".to_file("zh")
     File.exist?("人民广场到了.mp3").should be_true
     File.delete("人民广场到了.mp3")
   end
-  
+
+end
+
+describe 'set a server url' do
+  before(:all) do
+    Tts.server_url  "http://127.0.0.1:3001/translate_tts"
+  end
+
+  it "to_url should return a correct string" do
+    "hello".to_url("en").should == "http://127.0.0.1:3001/translate_tts?tl=en&q=hello"
+  end
+
+
+
 end
